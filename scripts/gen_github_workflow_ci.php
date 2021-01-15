26afca22652e1f579966787e0c2219267a7aaae7
Michael Wallner <mike@php.net>
2021-01-15 09:56:17 +0100

    travis -> gh-actions

---
+++ scripts/gen_github_workflow_ci.php
@@ -0,0 +1,76 @@
+#!/usr/bin/env php
+<?php echo "# generated file; do not edit!\n"; ?>
+
+name: ci
+on:
+  workflow_dispatch:
+  push:
+  pull_request:
+
+jobs:
+<?php
+
+$cur = "8.0";
+$gen = include __DIR__ . "/ci/gen-matrix.php";
+$job = $gen->github([
+"old-matrix" => [
+	"PHP" => ["5.6", "7.0", "7.1", "7.2", "7.3", "7.4"],
+	"enable_debug" => "yes",
+	"enable_maintainer_zts" => "yes",
+], 
+"master" => [
+	"PHP" => "master",
+	"enable_debug" => "yes",
+	"enable_zts" => "yes",
+],
+"cur-dbg-zts" => [
+	"PHP" => $cur,
+	"enable_debug",
+	"enable_zts",
+],
+"cur-cov" => [
+	"CFLAGS" => "'-O0 -g --coverage'",
+	"CXXFLAGS" => "'-O0 -g --coverage'",
+	"PHP" => $cur,
+]]);
+foreach ($job as $id => $env) {
+    printf("  %s:\n", $id);
+    printf("    name: %s\n", $id);
+    if ($env["PHP"] == "master") {
+        printf("    continue-on-error: true\n");
+    }
+    printf("    env:\n");
+    foreach ($env as $key => $val) {
+        printf("      %s: \"%s\"\n", $key, $val);
+    }
+?>
+    runs-on: ubuntu-20.04
+    steps:
+      - uses: actions/checkout@v2
+        with:
+          submodules: true
+      - name: Install
+        run: |
+          sudo apt-get install -y \
+            php-cli \
+            php-pear \
+            re2c
+      - name: Prepare
+        run: |
+          make -f scripts/ci/Makefile php || make -f scripts/ci/Makefile clean php
+      - name: Build
+        run: |
+          make -f scripts/ci/Makefile ext PECL=apfd
+      - name: Test
+        run: |
+          make -f scripts/ci/Makefile test
+<?php if (isset($env["CFLAGS"]) && strpos($env["CFLAGS"], "--coverage") != false) : ?>
+      - name: Coverage
+        if: success()
+        run: |
+          cd src/.libs
+          bash <(curl -s https://codecov.io/bash) -X xcode -X coveragepy
+<?php endif; ?>
+
+<?php
+}
